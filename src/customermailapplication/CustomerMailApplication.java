5fd35a972771b550f09ae75f9fba9a0e91b17c02
beabeabu <65439241+beabeabu@users.noreply.github.com>
2020-08-19 21:30:27 +0700

    Update CustomerMailApplication.java

--- src/customermailapplication/CustomerMailApplication.java
+++ src/customermailapplication/CustomerMailApplication.java
@@ -23,8 +23,16 @@ public class CustomerMailApplication {
             case "Regular":
                 customer = new RegularCustomer();
                 break;
+            
             //complete MountainCustomer
-            //complete DelinquentCustomer 
+            case "Mountain":
+                customer = new MountainCustomer();
+                break;
+        
+            //complete DelinquentCustomer
+            case "Delinquent":
+                customer = new DelinquentCustomer();
+                break;
         }
     }
     public String generateMail() {
